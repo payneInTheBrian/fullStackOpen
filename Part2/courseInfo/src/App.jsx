12d552cb23e4d28576c0019064e6d9d680cd3d7c
Brian Payne <payne.in.the.brian@gmail.com>
2025-01-27 17:16:52 -0600

    ex 2.5

--- Part2/courseInfo/src/App.jsx
+++ Part2/courseInfo/src/App.jsx
@@ -1,41 +1,4 @@
- const Header = ({ name }) => <h2>{name}</h2>
-
- const Total = ({ parts }) => {
-  const total = parts.reduce((s,p) => s + p.exercises, 0)
-  return(
-    <p>Number of exercises {total}</p>
-  )
- }
-
- const Part = ({ part, exercises }) => {
-   return (
-   <p>
-     {part} {exercises}
-     {console.log(part.name)}
-   </p>
-   )
- }
-
- const Content = ({ parts }) => {
-  return parts.map(part => (
-   <Part
-    part={part.name}
-    exercises={part.exercises} 
-    key={part.id}
-   />
-  ))
- }
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts}  />
-      
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
---
+++ Part2/courseInfo/src/components/Course.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+
+const Header = ({ name }) => {
+    return (
+      <>  
+        <h1>Web development curriculum</h1>
+        <h2>{name}</h2>
+      </>
+    )
+}
+const Total = ({ parts }) => {
+ const total = parts.reduce((s,p) => s + p.exercises, 0)
+ return(
+   <p>Number of exercises {total}</p>
+ )
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+  <p>
+    {part} {exercises}
+  </p>
+  )
+}
+
+const Content = ({ parts }) => {
+ return parts.map(part => (
+  <Part
+   part={part.name}
+   exercises={part.exercises} 
+   key={part.id}
+  />
+ ))
+}
+
+const Course = ({ course }) => {
+ return (
+   <>
+     <Header name={course.name}/>
+     <Content parts={course.parts} />
+     <Total parts={course.parts}  />
+     
+   </>
+ )
+}
+
+
+export default Course
