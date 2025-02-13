466779a976959714117c3f59f1d63e7d7045f92b
Brian Payne <payne.in.the.brian@gmail.com>
2025-02-12 23:57:16 -0600

    ex 2.12

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -8,20 +8,15 @@ import PersonForm from './components/PersonForm'
 import personService from './services/personService'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
     console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         console.log('promise fulfilled')
         setPersons(response.data)
@@ -34,20 +29,14 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      
     }
-    persons.find(person => person.name === newName) ? 
-      alert(`${newName} is already added to phonebook`) :
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
-          console.log(response)
-    })
-    setNewName('')
-    setNewNumber('')
+    personService
+      .create(personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+      })
+   
   }
 
   const handlePersonChange = (event) => {
